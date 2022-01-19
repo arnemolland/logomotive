090e84fe4469a3e1b856a0abae37e0ea765eb49c
Arne Molland <arne@molland.sh>
2022-01-19 22:05:23 +0100

    Exclude arm64 for macOS build, use adoptjdk 11 for android

--- server.go
+++ server.go
@@ -49,6 +49,7 @@ func (s *Server) Start() {
 		log.Fatalf("failed to connect to db: %v", err)
 	}
 	log.Print("connected to database")
+
 	logomotive.RegisterLogomotiveServiceServer(s.grpcSrv, NewLogomotive(client.Database("logs")))
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", s.port))
